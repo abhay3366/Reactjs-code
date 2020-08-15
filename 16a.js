e2908c402824928c1dc3c8fe09300b490933fa5f
abhay3366 <68136960+abhay3366@users.noreply.github.com>
2020-08-14 22:53:23 -0400

    Add files via upload
    new

---
+++ 16a.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+const name = "Abhay"
+const img1 ="https://picsum.photos/200";
+const img2 ="https://picsum.photos/200";
+const img3 ="https://picsum.photos/200";
+const link ="https://getbootstrap.com/docs/4.0/getting-started/introduction/";
+const header ={
+  color:"red",
+  fontSize:50,
+}
+ReactDOM.render(
+  <>
+   <h1 style ={{
+     color:"red",
+     fontSize:50,
+   }}> name={name}</h1>
+  <h1>Welcome to Abhay Image</h1>
+ 
+  <div className="img_div">
+    <img src={img1} alt = "imgae"/> 
+    <img src={img2} alt = "imgae"/>
+    <a href={link}>
+     <img src={img3} alt = "imgae"/>
+    </a>
+  </div>
+  
+  </>,
+  document.getElementById("root")
+);
